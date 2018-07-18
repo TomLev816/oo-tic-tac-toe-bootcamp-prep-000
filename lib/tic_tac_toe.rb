96ad7d88a630441694b0f95597fdfdb7c6b20794
Thomas Lever <thomaslever508@gmail.com>
2018-07-18 14:13:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,4 +39,17 @@ class TicTacToe
       turn()
     end
   end
+  def won?(board)
+    WIN_COMBINATIONS.each do |combo|
+      winner = combo
+
+      if @board[winner[0]] == "X" && @board[winner[1]] == "X" && @board[winner[2]] == "X"
+        return winner
+      end
+      if @board[winner[0]] == "O" && @board[winner[1]] == "O" && @board[winner[2]] == "O"
+        return winner
+      end
+    end
+    return false
+  end
 end
