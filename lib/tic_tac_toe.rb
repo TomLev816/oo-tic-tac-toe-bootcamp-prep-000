8e29610cc0d7e2bd09872c3364b8dce059ae952a
Thomas Lever <thomaslever508@gmail.com>
2018-07-18 13:58:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,4 +19,7 @@ class TicTacToe
   def position_taken?(index)
     @board[index] == "X" || @board[index] == "O"
   end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(@board, index)
+  end
 end
