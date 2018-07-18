7896ecea560900e40e573e3aaae9357e6c0af730
Thomas Lever <thomaslever508@gmail.com>
2018-07-18 14:12:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,4 +28,15 @@ class TicTacToe
   def current_player()
     turn_count() % 2 == 0 ? "X" : "O"
   end
+  def turn(board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      move(index, current_player(board))
+      display_board()
+    else
+      turn()
+    end
+  end
 end
